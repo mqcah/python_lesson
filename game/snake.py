de9416c509f68a3a2052d0af4939b5ecf619d4ca
mqcah <mqcha94@outlook.jp>
2020-09-30 22:49:38 +0900

    game snake

---
+++ game/snake.py
@@ -0,0 +1,57 @@
+import pygame
+
+width = 500 #
+height = 500 #
+rows #マス目の数
+
+
+class Cube():
+    def __init__(self, start, x=1, y=0, color(255, 0, 0))
+        pass
+
+    def move(self, x, y):
+        pass
+
+    def drow(self, surface, eyes=False):
+        pass
+
+
+class Snake():
+    def __init__(self, color, pos):
+        pass
+
+    def move(self):
+    pass
+
+    def reset(self, pos):
+    pass
+
+    def addbody(self):
+        pass
+
+    def drow(self, surface):
+        pass
+
+Window = pygame.display.set_mode((width, height))
+
+
+def reDrawWindow():
+    window.fill((0, 0, 0))
+
+
+def drowGrid(width, rows, surface)
+    pass
+
+def randomFood(rows, food):
+    pass
+
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT():
+                pygame.puit()
+
+        reDrawWindow()
+
+
+main()
